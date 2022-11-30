76664a49bc2a08ab8b676d1672050efef5eeb218
Jordan Rogers <j.rogers@wearedoubleline.com>
2022-11-29 20:54:09 -0600

    change encoding remove license lines throwing issues

--- src/migrations/AdminApp/202003300900-CreateHangfireSchemaAndTables.sql
+++ src/migrations/AdminApp/202003300900-CreateHangfireSchemaAndTables.sql
@@ -1,8 +1,3 @@
-﻿-- SPDX-License-Identifier: Apache-2.0
--- Licensed to the Ed-Fi Alliance under one or more agreements.
--- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
--- See the LICENSE and NOTICES files in the project root for more information.
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
